45b8a852ec060b3031f2b2869439c615dd3f60ca
Antoine <65553139+kysercodes@users.noreply.github.com>
2025-05-08 16:16:32 -0400

    testing the console

--- js/main.js
+++ js/main.js
@@ -40,7 +40,8 @@ document.addEventListener('DOMContentLoaded', function () {
   fetch('https://rlnauei8x0.execute-api.us-east-1.amazonaws.com/count')
       .then(response => response.json())
       .then(data => {
-          const parsed = JSON.parse(data.body); // <-- parse the body string
+        console.log(data.body)
+          // const parsed = JSON.parse(data.body); // <-- parse the body string
           const count = parsed.visitor_count;   // <-- now this is a real number
           counter.textContent = count;
           console.log("Visitor count:", count);
